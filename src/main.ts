dfaddc05c3032e1c8c32da35ce554a1fe7df4e9d
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-01-20 20:45:21 +0900

    Refactor;

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { MinecraftPacketIds } from "bdsx/bds/packetids";
 import { events } from "bdsx/event";
 
-events.packetSend(MinecraftPacketIds.Text).on((pkt, ni) => {
+events.packetSend(MinecraftPacketIds.Text).on((pkt) => {
+    const name = pkt.params.get(0);
     if (pkt.message === "§e%multiplayer.player.joined") {
-        const name = pkt.params.get(0);
         pkt.message = `[ §a+§r ] §7${name}`;
+        return;
     }
     if (pkt.message === "§e%multiplayer.player.left") {
-        const name = pkt.params.get(0);
         pkt.message = `[ §c-§r ] §7${name}`;
+        return;
     }
 });
 /*
